e1146527302dc3809b6da05f9e25f23976c162c3
prcy <phurichaya43@gmail.com>
2022-04-21 01:15:18 +0700

    you win page 50%

---
+++ public/win.js
@@ -0,0 +1,8 @@
+function sharefb() {
+    window.open("", "", "width=100,height=50");
+    // window.open("https://www.facebook.com/sharer/sharer.php?u=bkk-soisurvivor.netlify.app", "", "width=100,height=50");
+}
+
+function back() {
+
+}
