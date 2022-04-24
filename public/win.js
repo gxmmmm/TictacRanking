0fe5e036d7bd6a08ecf125d22f8bc4197cfa5158
prcy <phurichaya43@gmail.com>
2022-04-24 23:50:22 +0700

    edit you-win and add you-lose

--- public/win.js
+++ public/win.js
@@ -2,7 +2,3 @@ function sharefb() {
     window.open("", "", "width=100,height=50");
     // window.open("https://www.facebook.com/sharer/sharer.php?u=bkk-soisurvivor.netlify.app", "", "width=100,height=50");
 }
-
-// function back() {
-
-// }
